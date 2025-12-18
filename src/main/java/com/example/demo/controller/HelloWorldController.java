0102d1630bac1c18b55147f45feb98db1f6e8d1e
ayushmanom810-byte <ayushmanom810@gmail.com>
2025-12-18 22:26:10 +0530

    Update HelloWorldController.java

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @GetMapping("/messege")
 	public String getMessege() {	
-      return "hello-world";
+      return "hello2-world2";
 	}
 
 }
